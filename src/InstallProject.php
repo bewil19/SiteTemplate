f38a47d5e2f5a733f3c0bf8318c27e6bea960ffd
Be <be.wilson@benjamin-wilson.co.uk>
2024-08-20 15:03:35 +0100

    -

--- src/InstallProject.php
+++ src/InstallProject.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace bewil19\Site;
-use Composer\Script\Event;
 
 class InstallProject{
 
-    public static function postCreateProject(Event $event){
-        $projectFolder = str_replace("vendor" . DIRECTORY_SEPARATOR . "bewil19" . DIRECTORY_SEPARATOR . "sitetemplate" . DIRECTORY_SEPARATOR . "src", "", __DIR__);
-        $sourceFolder = __DIR__;
-        if(str_replace($projectFolder, "", $sourceFolder) === "vendor" . DIRECTORY_SEPARATOR . "bewil19" . DIRECTORY_SEPARATOR . "sitetemplate" . DIRECTORY_SEPARATOR . "src"){
+    public static function postCreateProject(){
+        $sourceFolder = dirname(__DIR__);
+        $projectFolder = str_replace("vendor" . DIRECTORY_SEPARATOR . "bewil19" . DIRECTORY_SEPARATOR . "sitetemplate", "", $sourceFolder);
+        
+        if(str_replace($projectFolder, "", $sourceFolder) === "vendor" . DIRECTORY_SEPARATOR . "bewil19" . DIRECTORY_SEPARATOR . "sitetemplate"){
             // do new project code
-            echo "Yes";
+            var_dump($sourceFolder);
+            var_dump($projectFolder);
         } else {
             echo "No";
         }
