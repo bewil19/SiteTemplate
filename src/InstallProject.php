a7046b5613fb93142e14cfc68c3d39701633231b
Be <be.wilson@benjamin-wilson.co.uk>
2024-08-20 14:49:46 +0100

    -

--- src/InstallProject.php
+++ src/InstallProject.php
@@ -6,6 +6,13 @@ use Composer\Script\Event;
 class InstallProject{
 
     public static function postCreateProject(Event $event){
-        echo __DIR__;
+        $projectFolder = str_replace("vendor" . DIRECTORY_SEPARATOR . "bewil19" . DIRECTORY_SEPARATOR . "sitetemplate" . DIRECTORY_SEPARATOR . "src", "", __DIR__);
+        $sourceFolder = __DIR__;
+        if(str_replace($projectFolder, "", $sourceFolder) === "vendor" . DIRECTORY_SEPARATOR . "bewil19" . DIRECTORY_SEPARATOR . "sitetemplate" . DIRECTORY_SEPARATOR . "src"){
+            // do new project code
+            echo "Yes";
+        } else {
+            echo "No";
+        }
     }
 }
